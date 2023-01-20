dee7f325c08f3a3a22512284d1013b9e05dc717b
heygyungyoon <yhg0498@coredax.com>
2023-01-20 19:33:13 +0900

    setting : HearoBanner 셋팅

---
+++ src/page/main/HeroBanner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Slider from "react-slick";
+
+function HereBanner() {
+  return (
+    <>
+      <Slider></Slider>
+    </>
+  );
+}
+
+export default HereBanner;
