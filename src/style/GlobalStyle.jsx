73538f0dcc981c5dcbdc32dbb1b7fe58e5addfc4
heygyungyoon <yhg0498@coredax.com>
2023-01-22 17:51:50 +0900

    font : pretendard 폰트 추가

---
+++ src/style/GlobalStyle.jsx
@@ -0,0 +1,155 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body,
+    div,
+    span,
+    applet,
+    object,
+    iframe,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    blockquote,
+    pre,
+    a,
+    abbr,
+    acronym,
+    address,
+    big,
+    cite,
+    code,
+    del,
+    dfn,
+    em,
+    img,
+    ins,
+    kbd,
+    q,
+    s,
+    samp,
+    small,
+    strike,
+    strong,
+    sub,
+    sup,
+    tt,
+    var,
+    b,
+    u,
+    i,
+    center,
+    dl,
+    dt,
+    dd,
+    ol,
+    ul,
+    li,
+    fieldset,
+    form,
+    label,
+    legend,
+    table,
+    caption,
+    tbody,
+    tfoot,
+    thead,
+    tr,
+    th,
+    td,
+    article,
+    aside,
+    canvas,
+    details,
+    embed,
+    figure,
+    figcaption,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    output,
+    ruby,
+    section,
+    summary,
+    time,
+    mark,
+    audio,
+    video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: "Pretendard";
+    font: inherit;
+    vertical-align: baseline;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article,
+    aside,
+    details,
+    figcaption,
+    figure,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    section {
+    display: block;
+    }
+    body {
+    line-height: 1;
+    min-height: 100vh;
+    min-height: calc(var(--vh) * 100);
+    }
+    ol,
+    ul,
+    li {
+    list-style: none;
+    text-decoration: none;
+    }
+    blockquote,
+    q {
+    quotes: none;
+    }
+    blockquote:before,
+    blockquote:after,
+    q:before,
+    q:after {
+    content: "";
+    content: none;
+    }
+    table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    }
+    *,
+    div,
+    p,
+    a,
+    span {
+    font-family: "Pretendard";
+    box-sizing: border-box;
+    }
+
+    a {
+    text-decoration: none;
+    color: inherit;
+    }
+
+    *::after, *::before {
+        content: "";
+        display: block;
+        position: absolute;
+    }
+    /* font */
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+`;
+
+export default GlobalStyle;
---
+++ src/style/theme.jsx
@@ -0,0 +1,52 @@
+import { mediaMax } from "../util/MediaQurey";
+
+const fontSize = {
+  lgfont: `
+    font-size: 18px;
+    ${mediaMax.sm} {
+        font-size: 16px;
+    }
+    `,
+  mdfont: `
+    font-size: 16px;
+    ${mediaMax.sm} {
+        font-size: 14px;
+    }
+    `,
+  smfont: `
+    font-size: 14px;
+    `,
+};
+
+const colors = {
+  bkgcolor: "#fff",
+  pointcolor: "#000",
+  deepgray: "#333",
+  maingray: "#aaa",
+  lightgray: "#ddd",
+  brightgray: "#eee",
+};
+
+const common = {
+  flexLeft: `
+    display:flex;
+    align-items: center;
+    `,
+  flexCenter: `
+    display:flex;
+    justify-content: center;
+    lign-items: center;
+    `,
+  flexBtween: `
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    `,
+};
+
+const theme = {
+  fontSize,
+  colors,
+  common,
+};
+export default theme;
