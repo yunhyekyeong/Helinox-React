0e2c40643ba2cd15b09819f1dbcc997b4c9ae5a9
heygyungyoon <yhg0498@coredax.com>
2023-01-24 00:22:47 +0900

    style: 공통레이아웃 Container style

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { mediaMax } from "../../util/MediaQurey";
+
+export const Section = styled.section`
+  margin: 100px 0;
+`;
+
+export const Container = styled.div`
+  width: 1280px;
+  margin: 0 auto;
+  @media (max-width: 1280px) {
+    width: calc(100% - 100px);
+  }
+  ${mediaMax.md} {
+    width: 100%;
+    padding: 0 16px;
+  }
+`;
