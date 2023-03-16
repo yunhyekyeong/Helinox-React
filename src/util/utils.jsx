2bdc5f143cf0106ccf6ad3e13f13eb1c5ea89a16
heygyungyoon <yhg0498@coredax.com>
2023-03-16 15:30:07 +0900

    feat : isMobile 함수 추가

--- src/util/utils.jsx
+++ src/util/utils.jsx
@@ -1,3 +1,5 @@
-export const isMobile = () => {
+const isMobile = () => {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 };
+
+export default isMobile;
